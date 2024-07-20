a6fd8cf59887faa582040afd8101250ac62592a6
Eric Gomes <ericvsg13@gmail.com>
2024-07-20 14:58:30 -0300

    Add script DataTables

--- LinkHub/Models/ApplicationUser.cs
+++ LinkHub/Models/ApplicationUser.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace LinkHub.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "Nome")]
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
     }
